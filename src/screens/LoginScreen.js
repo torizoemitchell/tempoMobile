699dbf52c68b8c3fd48d0691668e9ed4aa80036e
Tori Mitchell <tori.zoe.mitchell@gmail.com>
2018-12-28 16:04:31 -0700

    added styling to login page

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
-
+import { View, Text, Button, TextInput, StyleSheet, ImageBackground, Image } from 'react-native';
 import startMainTabs from './startMainTabs';
 
 class LoginScreen extends Component {
@@ -8,14 +7,55 @@ class LoginScreen extends Component {
         startMainTabs();
     }
 
+    signUpHandler = () => {
+        console.log("Sign Up pressed")
+    }
     render() {
         return (
-            <View>
-                <Text>Login Screen</Text>
-                <Button title="Login" onPress={this.loginHandler} />
+            
+            <View style={styles.container}>
+                <Text style={styles.brand}>Tempo</Text>
+                <View style={styles.inputContainer}>
+                    <TextInput placeholder="E-mail Address" style={styles.input} />
+                    <TextInput placeholder="Password" style={styles.input} secureTextEntry="true" />
+                </View>
+                <View style={styles.buttonContainer}>
+                    <Button title="Login" onPress={this.loginHandler} color="midnightblue" fontFamily="HelveticaNeue-UltraLight"/>
+                    <Button title="Sign-Up" onPress={this.signUpHandler} color="midnightblue" fontFamily="HelveticaNeue-UltraLight"/>
+                </View>
+                
             </View>
+    
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    inputContainer: {
+        width: "80%",
+    },
+    input: {
+        width: "100%",
+        borderColor: "grey",
+        borderWidth: 1,
+        padding: 10,
+        margin: 8,
+    },
+    buttonContainer: {
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center"      
+    },
+    brand: {
+        fontSize: 60,
+        fontFamily: "HelveticaNeue-UltraLight",
+    }
+})
 
 export default LoginScreen;
